c6cdd0745d0554cb2d333caace625ee9a526f66b
Brayan Gonzalez Marchena <brayan.gonzalez.marchena@cr.ey.com>
2023-04-26 01:47:44 -0400

    feat: initial commit

---
+++ app/src/deploy.js
@@ -0,0 +1,13 @@
+import { ethers } from 'ethers';
+import Escrow from './artifacts/contracts/Escrow.sol/Escrow';
+
+export default async function deploy(signer, arbiter, beneficiary, value) {
+  const factory = new ethers.ContractFactory(
+    Escrow.abi,
+    Escrow.bytecode,
+    signer
+  );
+  const escrowContract = await factory.deploy(arbiter, beneficiary, { value });
+  await escrowContract.deployed();
+  return escrowContract;
+}
